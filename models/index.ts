9b3de869f52f856266ad7e31d28cba5ecb737005
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-16 19:22:05 +0100

    Fix: Added all model to index model

---
+++ models/index.ts
@@ -0,0 +1,15 @@
+import { Buyer } from "./buyer.model";
+import { Merchant } from "./merchant.model";
+import { Order } from "./order.model";
+import { Payment } from "./payment.model";
+import { Product } from "./product.mode";
+import { PaymentInfo } from "./paymentInfo.model";
+
+export default {
+  Buyer,
+  Merchant,
+  Order,
+  Payment,
+  PaymentInfo,
+  Product,
+};
