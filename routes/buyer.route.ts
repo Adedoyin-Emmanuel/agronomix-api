9e8fc1937c6101117983df1bd94590c591bdf7cb
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-12-18 10:50:30 +0100

    Feat: Added merchant route

---
+++ routes/buyer.route.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { BuyerController } from "../controllers";
+import { useAuth, useCheckRole, useCreateUserLimiter } from "../middlewares";
+
+const buyerRouter = express.Router();
+
+buyerRouter.post("/", [useCreateUserLimiter], BuyerController.createBuyer);
+buyerRouter.get("/search", BuyerController.searchBuyer);
+buyerRouter.get("/online", BuyerController.getOnlineBuyers);
+buyerRouter.put(
+  "/:id",
+  [useAuth, useCheckRole("buyer")],
+  BuyerController.updateBuyer
+);
+buyerRouter.get("/me", [useAuth], BuyerController.getMe);
+buyerRouter.get("/", [useAuth], BuyerController.getAllBuyers);
+buyerRouter.get("/:id", [useAuth], BuyerController.getBuyerById);
+buyerRouter.delete(
+  "/:id",
+  [useAuth, useCheckRole("buyer")],
+  BuyerController.deleteBuyer
+);
+
+export default buyerRouter;
