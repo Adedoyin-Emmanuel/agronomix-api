2cb8108e5bb4b77671cf61cb0f483da06b013ee0
kaytee07 <kofi.taylor07@gmail.com>
2023-12-19 13:13:05 +0000

    create product and search product route created

--- controllers/index.ts
+++ controllers/index.ts
@@ -2,5 +2,6 @@ import HelloController from "./hello.controller";
 import AuthController from "./auth.controller";
 import BuyerController from "./buyer.controller";
 import MerchantController from "./merchant.controller";
+import ProductController from "./product.controller";
 
-export { HelloController, AuthController, BuyerController, MerchantController };
+export { HelloController, AuthController, BuyerController, MerchantController, ProductController };
