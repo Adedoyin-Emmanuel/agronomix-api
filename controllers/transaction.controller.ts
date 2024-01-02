b828682ea4e2a3f85308036dc0519086b1a3d191
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-02 01:10:53 +0100

    Feat: Finished Transaction Controller template

--- controllers/transaction.controller.ts
+++ controllers/transaction.controller.ts
@@ -8,6 +8,18 @@ class TransactionController {
 
     return response(res, 200, "Transaction initiated successfully");
   }
+
+  static async verify(req: Request, res: Response) {
+    const requestSchema = Joi.object({});
+
+    return response(res, 200, "Transaction verified successfully");
+  }
+
+  static async refund(req: Request, res: Response) {
+    const requestSchema = Joi.object({});
+
+    return response(res, 200, "Funds reimbursed successfully");
+  }
 }
 
 export default TransactionController;
