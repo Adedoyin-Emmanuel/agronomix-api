315d9f93ed28e30974de908b55c392b2be89ce20
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-02 01:16:23 +0100

    Feat: Finished transaction routes

---
+++ routes/transaction.route.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { TransactionController } from "../controllers";
+import { useAuth, useCheckRole } from "./../middlewares";
+
+const transactionRouter = express.Router();
+
+transactionRouter.post(
+  "/intiate",
+  [useAuth, useCheckRole("buyer")],
+  TransactionController.initiate
+);
+transactionRouter.post(
+  "/verify",
+  [useAuth, useCheckRole("buyer")],
+  TransactionController.verify
+);
+transactionRouter.post(
+  "/refund",
+  [useAuth, useCheckRole("buyer")],
+  TransactionController.refund
+);
+
+/**
+ * TODO Add transaction history for merchant and buyer
+ * That shouldn't be tough!
+ */
